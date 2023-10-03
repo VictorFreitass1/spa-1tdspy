1311c0b14b6adcff27b3c9059afa9ad06a5ceb5f
VictorFreitass <victor.fsilva45@gmail.com>
2023-10-02 21:02:04 -0300

    Continuando jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//ROTAS
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import Error404 from './routes/Error404.jsx'
 
+const routes = createBrowserRouter([
+    {path: "/", element: <App/>, errorElement:<Error404/>, children:[
+        {path: "/", element: <Home/>,},
+        {path:"/produtos",element:<Produtos/> },
+        { path:'/editar/produtos/:id', component :<EditarProdutos />}
+    ]}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </React.StrictMode>,
 )
--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom"
 
 export default function EditarProdutos() {
+
+    const{id} = useParams();
+    document.title = id;
+    
     return (
-      <div>EditarProdutos</div>
+      <div>
+        <h1>EditarProdutos</h1>
+        <h2>Produto Selecionado - {id}</h2>
+    </div>
     )
   }
   
